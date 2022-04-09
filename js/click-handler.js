2e06a9baec6b4b7cf79d0c34b0dc0575069f26e2
Greysoul <realgreysoul@mail.ru>
2022-04-09 16:48:26 +0000

    Update README.md, favicon.ico, material-icons.woff2, 404.html, index.html, js/case-handler.js, js/click-handler.js, js/load.js, js/material.min.js, js/save-handler.js, js/settings-handler.js, js/sound-handler.js, js/stats.js, js/upgrade-handler.js, js/util.js, js/cases/click-case.js, js/cases/sound-case.js, js/upgrades/auto-clicker.js, js/upgrades/click-bomb.js, js/upgrades/click-factory.js, js/upgrades/click-miner.js, js/upgrades/click-portal.js, js/upgrades/per-click.js, css/material.css, css/material.light_blue-indigo.min.css, css/material.light_blue-indigo.min.old.css, css/material-icons.css, css/style.css, css/w3.css

---
+++ js/click-handler.js
@@ -0,0 +1,1 @@
+var clickCount=document.getElementById("clickCount"),clicks=0,perClick=1,clickSound="Default",unlockedClickSounds=["Default"];document.getElementById("clicker").addEventListener("click",function(){clicks+=perClick,clickCount.innerText=`Чикенбеконов: ${clicks}`,totalClicks++,checkStats(),checkCases(),playSound(clickSound)});
---
+++ js/util.js
@@ -0,0 +1,1 @@
+const delay=e=>new Promise(t=>setTimeout(t,e));function generateNumberBetween(e,t){return Math.floor(Math.random()*(t-e+1))+e}
