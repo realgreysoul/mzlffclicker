fd41b64818576242a2018318b6daad7cb5dfd62d
Greysoul <realgreysoul@outlook.com>
2026-07-15 03:10:39 +0700

    Updated warningDialog, the associated script, and the related style

--- js/warning.js
+++ js/warning.js
@@ -3,9 +3,17 @@ window.addEventListener("DOMContentLoaded", function () {
     showModal(document.getElementById("warningDialog"));
     document.body.style.overflow = "hidden";
   }
-  document.getElementById("warningOkay").onclick = function () {
+  document.getElementById("warningYes").onclick = function () {
     localStorage.setItem("warningAccepted", "true");
     closeModal(document.getElementById("warningDialog"));
+    document.documentElement.classList.remove("warning-pending");
     document.body.style.overflow = "";
+  };
+  document.getElementById("warningNo").onclick = function () {
+    location.href =
+      "https://ya.ru/video/search?text=%D0%BC%D1%83%D0%BB%D1%8C%D1%82%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D1%8B";
+  };
+  document.getElementById("ageOkay").onclick = function () {
+    closeModal(document.getElementById("ageDialog"));
   };
 });
